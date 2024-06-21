bebd2fbc80f5ccb1c35e244db28e615bc0d0c475
Eddy <edkosyan2000@gmail.com>
2024-06-21 18:18:23 +0400

    add basic files

---
+++ src/api.js
@@ -0,0 +1,11 @@
+const express = require("express")
+//const planetsRouter = require("./routes/planets/planets.router")
+
+
+const api = express.Router()
+
+
+//Routes
+//api.use("/planets", planetsRouter)
+
+module.exports = api
---
+++ src/app.js
@@ -0,0 +1,21 @@
+const express = require("express")
+const cors = require("cors")
+const morgan = require("morgan")
+const api = require("./api")
+
+const app = express()
+
+//CORS
+app.use(cors({
+    origin: "http://localhost:3000"
+}))
+
+//Loging
+app.use(morgan("combined"))
+//JSON converting
+app.use(express.json())
+//API
+app.use("/v1", api)
+
+
+module.exports = app
