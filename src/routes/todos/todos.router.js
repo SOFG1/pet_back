30a4962dd8c29c3c6b965d18d68a6907ca04d22b
Eddy <edkosyan2000@gmail.com>
2024-06-22 01:16:52 +0400

    check auth for todos

--- src/routes/todos/todos.router.js
+++ src/routes/todos/todos.router.js
@@ -2,13 +2,14 @@ const express = require("express");
 const todosController = require("./todos.controller");
 const { addTodoValidator } = require("./todos.validators");
 const { handleValidationErrors } = require("../../utils/handleValidationErrors");
+const { checkAuth } = require("../../utils/checkAuth");
 
 const todosRouter = express.Router();
 
-todosRouter.get("/", todosController.httpGetAllTodos);
-todosRouter.post("/", addTodoValidator, handleValidationErrors, todosController.httpAddTodo);
-todosRouter.delete("/:id", todosController.httpDeleteTodo);
-todosRouter.patch("/:id",addTodoValidator, handleValidationErrors, todosController.httpEditTodo);
+todosRouter.get("/", checkAuth, todosController.httpGetAllTodos);
+todosRouter.post("/", checkAuth, addTodoValidator, handleValidationErrors, todosController.httpAddTodo);
+todosRouter.delete("/:id", checkAuth, todosController.httpDeleteTodo);
+todosRouter.patch("/:id", checkAuth, addTodoValidator, handleValidationErrors, todosController.httpEditTodo);
 
 
 
