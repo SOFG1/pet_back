5f3f81af43a41ae75c92f5ba23c91c123d88efc8
Eddy <edkosyan2000@gmail.com>
2024-06-21 21:18:53 +0400

    add todo text validation

--- src/utils/handleValidationErrors.js
+++ src/utils/handleValidationErrors.js
@@ -4,7 +4,7 @@ const { validationResult } = require("express-validator")
 const handleValidationErrors = (req, res, next) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
-      return res.status(400).json(errors.array());
+      return res.status(400).json(errors.array().map(e => e.msg));
     }
     next()
 }
