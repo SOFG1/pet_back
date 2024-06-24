28a43cf5cea6d4bfb34fc95ece47e822fa170dcc
Eddy <edkosyan2000@gmail.com>
2024-06-24 14:19:02 +0400

    change experience terms

--- src/routes/users/users.controller.js
+++ src/routes/users/users.controller.js
@@ -27,7 +27,7 @@ async function httpSetLike(req, res) {
 async function increaseUsersExperience() {
   setInterval(() => {
     increaseAllUsersExp();
-  }, 1000 * 60 * 60 * 24);
+  }, 1000 * 60 * 60);
 }
 
 module.exports = {
